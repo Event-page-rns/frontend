327da0c4416cda5c7946eace5ed66d1e2d957e85
Kishan Pal <kishanpalh6145@gmail.com>
2024-07-26 00:16:30 +0530

    integrated login, register api

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-
+import { DocumentContext } from "../Contexts/DocumentContext";
+import { useContext, React, useState } from "react";
 
 const Login = () => {
+  const { login } = useContext(DocumentContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email + " " + password);
+    login(email, password);
+  };
+
   return (
-    <div className='h-[80vh] flex justify-center items-center'>
-      <div className='flex justify-center items-center border-2 border-black rounded-md p-4'>
+    <div className="h-[80vh] flex justify-center items-center">
+      <div className="flex justify-center items-center border-2 border-black rounded-md p-4">
         <div className="max-w-md relative flex flex-col p-4 rounded-md text-black bg-white">
           <div className="text-2xl font-bold mb-2 text-black text-center">
             Welcome back to <span className="text-red-500">Eventus</span>
@@ -12,7 +22,7 @@ const Login = () => {
           <div className="text-sm font-normal mb-4 text-center text-[#1e0e4b]">
             Log in to your account
           </div>
-          <form className="flex flex-col gap-3">
+          <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
             <div className="block relative">
               <label
                 htmlFor="email"
@@ -23,6 +33,8 @@ const Login = () => {
               <input
                 type="text"
                 id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 className="rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2 ring-gray-900 outline-0"
               />
             </div>
@@ -36,6 +48,8 @@ const Login = () => {
               <input
                 type="password"
                 id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 className="rounded border border-gray-200 text-sm w-full font-normal leading-[18px] text-black tracking-[0px] appearance-none block h-11 m-0 p-[11px] focus:ring-2 ring-offset-2 ring-gray-900 outline-0"
               />
             </div>
@@ -52,7 +66,7 @@ const Login = () => {
             </button>
           </form>
           <div className="text-sm text-center mt-[1.6rem]">
-            Don’t have an account yet?{' '}
+            Don’t have an account yet?{" "}
             <a className="text-sm text-red-500" href="#">
               Sign up for free!
             </a>
